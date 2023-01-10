66a6de12d51257c494318635ddb0edcde4d2ae86
kcy013100 <kcy013100@naver.com>
2023-01-10 13:49:59 +0900

    2023/01/10

--- Baekjoon/3단계/5번.py
+++ Baekjoon/3단계/5번.py
@@ -1,4 +1,5 @@
-t = int(input()) # input 대신 sys.stdin.readlined을 사용하면 시간 단축
+import sys
+t = int(sys.stdin.readline()) # input 대신 sys.stdin.readlined을 사용하면 시간 단축
 
 for i in range(t):
     a, b = map(int, input().split())
